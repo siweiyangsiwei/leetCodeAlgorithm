5881dcb4aae123b491f08e18f22cbdda7e10b9a1
Xavier <Xavier@qq.com>
2022-10-21 21:56:49 +0800

    2022-10-21二叉树序列化与反序列化的学习

---
+++ 22-10-21-2.java
@@ -0,0 +1,61 @@
+// leetcode:297
+// 序列化是将一个数据结构或者对象转换为连续的比特位的操作，进而可以将转换后的数据存储在一个文件或者内存中，同时也可以通过网络传输到另一个计算机环境，采取相反方式重构得到原数据。
+
+// 请设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 / 反序列化算法执行逻辑，你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构。
+
+// 提示: 输入输出格式与 LeetCode 目前使用的方式一致，详情请参阅 LeetCode 序列化二叉树的格式。你并非必须采取这种方式，你也可以采用其他的方法解决这个问题。
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+    StringBuilder str = new StringBuilder();
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        tarverse(root);
+        return str.toString();
+    }
+
+    public void tarverse(TreeNode root){
+        if(root == null){
+            str.append("#").append(",");
+            return ;
+        }
+        tarverse(root.left);
+        tarverse(root.right);
+        str.append(root.val).append(",");
+
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        LinkedList<String> nodes = new LinkedList<>();
+        for(String s: data.split(",")){
+            nodes.addLast(s);
+        }
+        return deserialize(nodes);
+    }
+
+    public TreeNode deserialize(LinkedList<String> nodes){
+        if(nodes.isEmpty()) return null;
+        String first = nodes.removeLast();
+        if(first.equals("#")) return null;
+        TreeNode root = new TreeNode(Integer.parseInt(first));
+        root.right= deserialize(nodes);
+        root.left = deserialize(nodes);
+        return root;
+
+    }
+
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
